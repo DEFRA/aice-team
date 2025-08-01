dfb426c5610653815f47a9ef4bcff8d16f61cd1b
sfitz42 <87677173+sfitz42@users.noreply.github.com>
2025-08-01 14:05:17 +0100

    Fix async generation issue

---
+++ tech-radar/radar/constants.js
@@ -0,0 +1,40 @@
+const RADAR_QUADRANT_NAMES = {
+  TECHNIQUES: 'Techniques',
+  FRAMEWORKS: 'Frameworks',
+  TOOLS: 'Tools',
+  PLATFORMS: 'Platforms'
+}
+
+const RADAR_RING_NAMES = {
+  ADOPT: 'Adopt',
+  TRIAL: 'Trial',
+  ASSESS: 'Assess',
+  HOLD: 'Hold'
+}
+
+const RADAR_QUADRANT_MAP = Object.keys(RADAR_QUADRANT_NAMES).reduce((map, key, index) => {
+  map[RADAR_QUADRANT_NAMES[key]] = index
+
+  return map
+}, {})
+
+const RADAR_RING_MAP = Object.keys(RADAR_RING_NAMES).reduce((map, key, index) => {
+  map[RADAR_RING_NAMES[key]] = index
+
+  return map
+}, {})
+
+const RADAR_RING_COLOUR_MAP = {
+  [RADAR_RING_NAMES.ADOPT]: '#4CAF50',
+  [RADAR_RING_NAMES.TRIAL]: '#FFC107',
+  [RADAR_RING_NAMES.ASSESS]: '#2196F3',
+  [RADAR_RING_NAMES.HOLD]: '#F44336'
+}
+
+export {
+  RADAR_QUADRANT_NAMES,
+  RADAR_RING_NAMES,
+  RADAR_QUADRANT_MAP,
+  RADAR_RING_MAP,
+  RADAR_RING_COLOUR_MAP
+}
--- tech-radar/scripts/generate.js
+++ tech-radar/scripts/generate.js
@@ -1,56 +1,9 @@
-import { JSDOM } from "jsdom"
-import data from "../data/radar.json" with { type: "json" }
-import { radar_visualization } from "../lib/radar.js"
-function main() {  
-    console.log(data.entries)
+import data from '../data/radar.json' with { type: 'json' }
 
-    const document = new JSDOM().window.document
-    console.log("generating radar")
+import { buildRadar } from '../radar/radar-builder.js'
 
-    radar_visualization(
-        document,
-        {
-  repo_url: "https://github.com/zalando/tech-radar",
-  svg_id: "radar",
-  width: 1450,
-  height: 1000,
-  scale: 1.0,
-  colors: {
-    background: "#fff",
-    grid: "#bbb",
-    inactive: "#ddd"
-  },
-  // Some font families might lead to font size issues
-  // Arial, Helvetica, or Source Sans Pro seem to work well though
-  font_family: "Arial, Helvetica",
-  title: "My Radar",
-  quadrants: [
-    { name: "Bottom Right" },
-    { name: "Bottom Left" },
-    { name: "Top Left" },
-    { name: "Top Right" }
-  ],
-  rings: [
-    { name: "INNER",  color: "#5ba300" },
-    { name: "SECOND", color: "#009eb0" },
-    { name: "THIRD",  color: "#c7ba00" },
-    { name: "OUTER",  color: "#e09b96" }
-  ],
-  print_layout: true,
-  links_in_new_tabs: true,
-  entries: [
-   {
-      label: "Some Entry",
-      quadrant: 3,          // 0,1,2,3 (counting clockwise, starting from bottom right)
-      ring: 2,              // 0,1,2,3 (starting from inside)
-      moved: -1             // -1 = moved out (triangle pointing down)
-                            //  0 = not moved (circle)
-                            //  1 = moved in  (triangle pointing up)
-                            //  2 = new       (star)
-   },
-    // ...
-  ]
-});
-console.log("genration complete")
+async function main() {
+  const rawSvg = await buildRadar(data.entries)
 }
-main()
+
+await main()
